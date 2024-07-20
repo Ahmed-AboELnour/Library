2545484c044f5af82b148aacc61176cba108ea58
154751 <ahmed.elsayedabuelnour@arrow.com>
2024-07-20 14:54:40 +0300

    Done

--- src/main/java/com/library/config/SecurityConfig.java
+++ src/main/java/com/library/config/SecurityConfig.java
@@ -39,10 +39,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
+        /**
+         * For mvn test
+         */
 //        http
 //                .csrf().disable() // Disable CSRF for tests
 //                .authorizeRequests()
 //                .anyRequest().permitAll();
+
+
 
         http.csrf().disable()
                 .headers().frameOptions().disable().disable()
